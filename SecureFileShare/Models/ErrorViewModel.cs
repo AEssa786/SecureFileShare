e1efd14ab06738dca53e457a10f6b8c717c46b2a
Abdullah Essa <aessa78644@gmail.com>
2026-04-03 21:31:07 +0200

    Add project files.

---
+++ SecureFileShare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecureFileShare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
